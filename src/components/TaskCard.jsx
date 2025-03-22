0a2b68e89b29e87740bd67249f81b146b3e8b278
Maxamed459 <maxamedmahdi459@gmail.com>
2025-03-22 11:45:08 +0300

    add TaskCard component.

---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Tag from './Tag'
+import deleteIcon from '../assets/delete.png'
+
+const TaskCard = () => {
+  return (
+    <article className='border-1 border-[#dcdcdc] rounded-[10px] w-full min-h-[100px] p-[15px] my-[15px] mx-0'>
+        <p className='text-[18px] font-[600] mb-[15px]'>This is sample text.</p>
+
+        <div className='flex justify-between items-center'>
+            <div>
+                <Tag tag="HTML"/>
+                <Tag tag="CSS"/>
+            </div>
+            <div className='w-[35px] h-[35px] rounded-full flex items-center justify-center hover:bg-[#ebebeb] transition duration-300 ease-in-out'>
+                <img src={ deleteIcon } alt="" className='w-[25px] opacity-[50%] hover:opacity-[80%] transition duration-300 ease-in-out' />
+            </div>
+        </div>
+    </article>
+  )
+}
+
+export default TaskCard
